fff2f2f7c75e3af3b2f777e0e46b87a2b4376a27
abersheeran <me@abersheeran.com>
2020-10-21 04:31:22 +0800

    Update

--- a2wsgi/asgi.py
+++ a2wsgi/asgi.py
@@ -1,11 +1,10 @@
 import asyncio
+import collections
 import threading
-from enum import Enum
 from http import HTTPStatus
-from typing import cast, Iterable, Optional
+from typing import Any, Iterable
 
 from .types import (
-    ExcInfo,
     Message,
     Scope,
     Environ,
@@ -16,19 +15,54 @@ from .types import (
 __all__ = ("ASGIMiddleware",)
 
 
+class AsyncEvent:
+    def __init__(self, loop: asyncio.AbstractEventLoop = None) -> None:
+        if loop is None:
+            loop = asyncio.get_event_loop()
+        self.loop = loop
+        self.__waiters = collections.deque()
+        self.__nowait = False
+
+    def set(self, message: Any) -> None:
+        self.__message = message
+        for future in self.__waiters:  # type: asyncio.Future
+            if future.done():
+                continue
+            future.set_result(message)
+
+    async def wait(self) -> Any:
+        if self.__nowait:
+            return None
+
+        future = self.loop.create_future()
+        self.__waiters.append(future)
+        try:
+            result = await future
+            return result
+        finally:
+            self.__waiters.remove(future)
+
+    def set_nowait(self) -> None:
+        self.__nowait = True
+
+
+class SyncEvent:
+    def __init__(self) -> None:
+        self.__event = threading.Event()
+        self.__message: Any = None
+
+    def set(self, message: Any) -> None:
+        self.__message = message
+        self.__event.set()
+
+    def wait(self) -> Any:
+        self.__event.wait()
+        self.__event.clear()
+        message, self.__message = self.__message, None
+        return message
+
+
 def build_scope(environ: Environ) -> Scope:
-    scope = {
-        "type": "http",
-        "asgi": {"version": "3.0", "spec_version": "3.0"},
-        "http_version": environ.get("SERVER_PROTOCOL", "http/1.0").split("/")[1],
-        "method": environ["REQUEST_METHOD"],
-        "scheme": environ.get("wsgi.url_scheme", "http"),
-        "path": environ["PATH_INFO"].encode("latin1").decode("utf8"),
-        "query_string": environ["QUERY_STRING"].encode("ascii"),
-        "root_path": environ.get("SCRIPT_NAME", "").encode("latin1").decode("utf8"),
-        "client": None,
-        "server": (environ["SERVER_NAME"], int(environ["SERVER_PORT"])),
-    }
     headers = [
         (
             each[5:].lower().replace("_", "-").encode("latin1"),
@@ -41,9 +75,20 @@ def build_scope(environ: Environ) -> Scope:
         headers.append((b"content-type", environ["CONTENT_TYPE"].encode("latin1")))
     if environ.get("CONTENT_LENGTH"):
         headers.append((b"content-length", environ["CONTENT_LENGTH"].encode("latin1")))
-    scope["headers"] = headers
 
-    return scope
+    return {
+        "type": "http",
+        "asgi": {"version": "3.0", "spec_version": "3.0"},
+        "http_version": environ.get("SERVER_PROTOCOL", "http/1.0").split("/")[1],
+        "method": environ["REQUEST_METHOD"],
+        "scheme": environ.get("wsgi.url_scheme", "http"),
+        "path": environ["PATH_INFO"].encode("latin1").decode("utf8"),
+        "query_string": environ["QUERY_STRING"].encode("ascii"),
+        "root_path": environ.get("SCRIPT_NAME", "").encode("latin1").decode("utf8"),
+        "client": None,
+        "server": (environ["SERVER_NAME"], int(environ["SERVER_PORT"])),
+        "headers": headers,
+    }
 
 
 class ASGIMiddleware:
@@ -75,12 +120,6 @@ class ASGIMiddleware:
         )
 
 
-class ASGIState(Enum):
-    RECEIVE = "R"
-    SEND = "S"
-    ERROR = "E"
-
-
 class ASGIResponder:
     def __init__(
         self, loop: asyncio.AbstractEventLoop, app: ASGIApp, wait_time: float = None
@@ -88,116 +127,93 @@ class ASGIResponder:
         self.loop = loop
         self.app = app
         self.wait_time = wait_time
-        self.sync_event = threading.Event()
-        self.async_event: asyncio.Event
-        self.loop.call_soon_threadsafe(self._init_async_event)
-        self.state: Optional[ASGIState] = None
-        self.body = bytearray()
-        self.more_body = True
-        self.exception: Optional[ExcInfo] = None
-        self.asgi_msg: Optional[Message] = None
 
-    def _done_callback(self, future: asyncio.Future) -> None:
-        if future.exception():
-            e = future.exception()
-            self.exception = type(e), e, e.__traceback__
-            self.state = ASGIState.ERROR
-        self.sync_event.set()
-
-    def _init_async_event(self) -> None:
-        """
-        Must init asyncio.Event in a loop.
-
-        https://docs.python.org/3/library/asyncio-sync.html#asyncio.Event
-        """
-        if not hasattr(self, "async_event"):
-            self.async_event = asyncio.Event()
+        self.sync_event = SyncEvent()
+        self.async_event = AsyncEvent(loop)
 
     def __call__(
         self, environ: Environ, start_response: StartResponse
     ) -> Iterable[bytes]:
-        scope = build_scope(environ)
+
+        asgi_done = threading.Event()
+
+        def _done_callback(future: asyncio.Future) -> None:
+            if future.exception() is not None:
+                e: BaseException = future.exception()  # type: ignore
+                self.sync_event.set(
+                    {"type": "error", "exception": (type(e), e, e.__traceback__)}
+                )
+            asgi_done.set()
+
         run_asgi: asyncio.Task = self.loop.create_task(
-            self.app(scope, self.receive, self.send)
+            self.app(build_scope(environ), self.receive, self.send)
         )
-        run_asgi.add_done_callback(self._done_callback)
+        run_asgi.add_done_callback(_done_callback)
+
         read_count, body = 0, environ["wsgi.input"]
-        content_length = int(environ.get("CONTENT_LENGTH", 0))
-        self.more_body = content_length > 0
-        self.loop.call_soon_threadsafe(lambda: None)  # call loop to run
-        while True:  # do while
-            self.sync_event.wait()
-            self.sync_event.clear()
-            if self.state == ASGIState.RECEIVE:
+        content_length = int(environ.get("CONTENT_LENGTH", None) or 0)
+
+        while True:
+            message = self.sync_event.wait()
+            message_type = message["type"]
+            if message_type == "receive":
                 data = body.read(min(16384, content_length - read_count))
-                self.body.extend(data)
                 read_count += len(data)
-                self.more_body = read_count < content_length
-            elif self.state == ASGIState.SEND:
-                message = cast(Message, self.asgi_msg)
-                self.asgi_msg = None
-                if message["type"] == "http.response.start":
-                    status = message["status"]
-                    headers = [
-                        (
-                            name.strip().decode("latin1"),
-                            value.strip().decode("latin1"),
-                        )
-                        for name, value in message["headers"]
-                    ]
-                    start_response(
-                        f"{status} {HTTPStatus(status).phrase}", headers, None
+                self.async_event.set(
+                    {
+                        "type": "http.request",
+                        "body": data,
+                        "more_body": read_count < content_length,
+                    }
+                )
+            elif message_type == "http.response.start":
+                status = message["status"]
+                headers = [
+                    (
+                        name.strip().decode("latin1"),
+                        value.strip().decode("latin1"),
                     )
-                elif message["type"] == "http.response.body":
-                    yield message.get("body", b"")
-                    if not message.get("more_body", False):
-                        break
-                elif message["type"] == "http.response.disconnect":
+                    for name, value in message["headers"]
+                ]
+                start_response(f"{status} {HTTPStatus(status).phrase}", headers, None)
+                self.async_event.set(None)
+            elif message_type == "http.response.body":
+                yield message.get("body", b"")
+                more_body = message.get("more_body", False)
+                if not more_body:
+                    self.async_event.set_nowait()
+                self.async_event.set(None)
+                if not more_body:
                     break
-                else:
-                    run_asgi.cancel()
-                    raise RuntimeError("What's wrong with the ASGI app?")
-            elif self.state == ASGIState.ERROR:
+            elif message_type == "http.response.disconnect":
+                self.async_event.set_nowait()
+                self.async_event.set(None)
+                break
+            elif message_type == "error":
                 start_response(
                     f"{500} {HTTPStatus(500).phrase}",
                     [
                         ("Content-Type", "text/plain; charset=utf-8"),
                         ("Content-Length", str(len(HTTPStatus(500).description))),
                     ],
-                    self.exception,
+                    message["exception"],
                 )
                 yield str(HTTPStatus(500).description).encode("utf-8")
+                self.async_event.set_nowait()
+                self.async_event.set(None)
                 break
 
-            if run_asgi.done() and self.state is None:
+            if run_asgi.done():
                 break
-            self.state = None
-            self.loop.call_soon_threadsafe(
-                lambda event: (event.set(), event.clear()), self.async_event
-            )
+
         # HTTP response ends, wait for run_asgi's background tasks
-        self.loop.call_soon_threadsafe(self.async_event.set)
-        if not run_asgi.done():
-            self.sync_event.wait(self.wait_time)
+        asgi_done.wait(self.wait_time)
         run_asgi.cancel()
 
     async def receive(self) -> Message:
-        if not self.more_body:
-            return {"type": "http.request", "body": b"", "more_body": False}
-
-        self.state = ASGIState.RECEIVE
-        self.sync_event.set()
-        await self.async_event.wait()
-        message = {
-            "type": "http.request",
-            "body": bytes(self.body),
-            "more_body": self.more_body,
-        }
-        del self.body[:]
-        return message
+        self.sync_event.set({"type": "receive"})
+        return await self.async_event.wait()
 
     async def send(self, message: Message) -> None:
-        self.state = ASGIState.SEND
-        self.asgi_msg = message
-        self.sync_event.set()
+        self.sync_event.set(message)
         await self.async_event.wait()
