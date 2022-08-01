57252044fdbf3507f46b9b352d8bfa8f377a19ef
krieu <kawakami@atama.plus>
2022-08-01 12:21:09 +0900

    Fix stream type

--- a2wsgi/asgi.py
+++ a2wsgi/asgi.py
@@ -1,8 +1,8 @@
 import asyncio
 import collections
-from io import StringIO
 import threading
 from http import HTTPStatus
+from io import BytesIO
 from itertools import chain
 from typing import Any, Deque, Iterable
 
@@ -175,7 +175,7 @@ class ASGIResponder:
         )
         run_asgi.add_done_callback(_done_callback)
 
-        read_count, body = 0, environ.get("wsgi.input", StringIO())
+        read_count, body = 0, environ["wsgi.input"] or BytesIO()
         content_length = int(environ.get("CONTENT_LENGTH", None) or 0)
 
         self.loop.call_soon_threadsafe(lambda: None)
